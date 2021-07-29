1cc6ca7feaad91dea53b83e87ce0e38ca478400d
mustafa fathi <mustafa.fathi7@gmail.com>
2021-07-29 08:12:36 +0200

    add list, movie & user models

---
+++ api/models/List.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const ListSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, uniqe: true },
+    type: { type: String},
+    genre: { type: String},
+    content: {type: Array}
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", ListSchema)
